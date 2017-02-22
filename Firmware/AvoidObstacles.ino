61f4df9770e7b84c57201b5cb0b09e7ab8cee38c
Amrit Diggavi Seshadri <s1443575@sms.ed.ac.uk>
2017-02-22 16:03:03 +0000

    logic changed

--- Firmware/AvoidObstacles.ino
+++ Firmware/AvoidObstacles.ino
@@ -3,6 +3,7 @@
 #include <Wire.h>
 #include <Arduino.h>
 
+
 #define FRONT 2
 #define RIGHT 3
 #define BACK 4
@@ -26,19 +27,6 @@ void test(){
   run = 1;
 }
 
-void dontMove(){
-  motorStop(FRONT);
-  motorStop(BACK);
-  motorStop(LEFT);
-  motorStop(RIGHT);
-}
-
-void spinmotor(){
-  int motor = atoi(sCmd.next());
-  int power = atoi(sCmd.next());
-  motorForward(motor, power);
-}
-
 void motorControl(int motor, int power){
   if(power == 0){
     motorStop(motor);
@@ -49,45 +37,11 @@ void motorControl(int motor, int power){
   }
 }
 
-void rationalMotors(){
-  int front = atoi(sCmd.next());
-  int back  = atoi(sCmd.next());
-  int left  = atoi(sCmd.next());
-  int right = atoi(sCmd.next());
-  motorControl(FRONT, front);
-  motorControl(BACK, back);
-  motorControl(LEFT, left);
-  motorControl(RIGHT, right);
-}
-
-void pingMethod(){
-  Serial.println("pang");
-}
-
-void kicker(){
-  int type = atoi(sCmd.next());
-  if(type == 0){
-    motorStop(KICKERS);
-  } else if (type == 1){
-    motorForward(KICKERS, 100);
-  } else {
-    motorBackward(KICKERS, 100);
-  }
-}
-
-
-
 void completeHalt(){
   motorAllStop();
 }
 
 void setup(){
-  sCmd.addCommand("f", dontMove); 
-  sCmd.addCommand("h", completeHalt); 
-  sCmd.addCommand("motor", spinmotor); 
-  sCmd.addCommand("r", rationalMotors); 
-  sCmd.addCommand("ping", pingMethod); 
-  sCmd.addCommand("kick", kicker); 
   SDPsetup();
   helloWorld();
 }
@@ -193,23 +147,32 @@ void avoid_obstacles()
   long L=sensorRead('l');
   long R=sensorRead('r');
 
-  if(F<10||F>2000)
+if(F<10||B<10)
+{
+  if(F<10)
   {
     //Move backwards.
     motorControl(FRONT,-50);
     motorControl(BACK,50);
     Serial.println("Obstacle_In_Front");
   }
-
-  if(B<10||B>2000)
+  else if(B<10)
   {
     //Move forward
     motorControl(FRONT,50);
     motorControl(BACK,-50);
     Serial.println("Obstacle_In_Back");
   }
+}
+else
+{
+   motorControl(FRONT,0);
+   motorControl(BACK,0);
+}
 
-  if(L<10||L>2000)
+if(L<10||R<10)
+{
+  if(L<10)
   {
     //Move right
     motorControl(RIGHT,50);
@@ -218,7 +181,7 @@ void avoid_obstacles()
 
   }
 
-  if(R<10||R>2000)
+  else if(R<10)
   {
     //Move left
     motorControl(RIGHT,-50);
@@ -226,7 +189,11 @@ void avoid_obstacles()
     Serial.println("Obstacle_In_Right");
   }
 
-
+}
+else
+{
+   motorControl(RIGHT,0);
+   motorControl(LEFT,0);
 
 }
 
